02c7d58227edb255d5ece7a8849dbf5e3548765e
ezestimateshq-debug <ez.estimateshq@gmail.com>
2026-06-08 19:26:55 -0500

    Wire live demo form and update contact details

--- site-config.js
+++ site-config.js
@@ -1,7 +1,7 @@
 const EZ_ESTIMATES_CONFIG = {
   demoFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSe-1V7lapHXlHCkdHLYJtfNWVMc6KwrdI6aMpdHjzJRgiW7rA/viewform?usp=dialog",
   sampleProposalUrl: "assets/sample-proposal.pdf",
-  salesOnePagerUrl: "assets/sales-one-pager.pdf",
+  salesOnePagerUrl: "sales-one-pager.pdf",
   ownerName: "Gavin Whitaker",
   contactEmail: "ez.estimateshq@gmail.com",
   contactEmailUrl: "mailto:ez.estimateshq@gmail.com",
